85910cf1b5504e38e72ec65005e5a0ca1b66ee5b
John Brunelle <john_brunelle@harvard.edu>
2013-12-19 12:49:29 -0500

    introducing wrappers for multi-Comp and multi-MPI builds

--- setup.sh
+++ setup.sh
@@ -1,24 +1,25 @@
-#the build host
-export FASRCSW_BUILD_HOST=hero4502
+#--- basic config -- adjust as needed
 
 #the location of production fasrcsw clone
 export FASRCSW_PROD=/n/sw/fasrcsw
 
+#default compilers and mpi stacks
+#update these as versions increase.
+#this assumes each FASRCSW_MPIS has been built with each FASRCSW_COMPS
+FASRCSW_COMPS=(
+	gcc/4.8.1-fasrc01
+	intel/13.0.079-fasrc01
+)
+FASRCSW_MPIS=(
+	openmpi/1.7.2-fasrc01
+	mvapich2/2.0b-fasrc01
+)
 
-#---
+#the build host
+export FASRCSW_BUILD_HOST=hero4502
 
 
-#check that we're on the build host
-if [ -z "${FASRCSW_NO_BUILD_HOST_CHECK:-}" ] && [ "$(hostname -s)" != "$FASRCSW_BUILD_HOST" ]; then
-	echo "*** ERROR *** you are not logged into the build host, $FASRCSW_BUILD_HOST  (set \$FASRCSW_NO_BUILD_HOST_CHECK to something non-empty to override this)" >&2
-	return 1
-fi
-
-#check that we're on the build host
-if [ -z "${FASRCSW_NO_DEV_EQ_PROD_CHECK:-}" ] && [ "$FASRCSW_DEV" = "$FASRCSW_PROD" ]; then
-	echo "*** ERROR *** \$FASRCSW_DEV and \$FASRCSW_PROD are the same (set \$FASRCSW_NO_DEV_EQ_PROD_CHECK to something non-empty to override this)" >&2
-	return 1
-fi
+#--- environment setup
 
 #set the location of this clone
 if [ -z "$BASH_SOURCE" ]; then
@@ -27,3 +28,4 @@ if [ -z "$BASH_SOURCE" ]; then
 fi
 export FASRCSW_DEV="$(dirname "$(readlink -e "$BASH_SOURCE")")"  #(the abs path of the dir containing this setup.sh)
 
+export PATH="$FASRCSW_DEV/bin:$PATH"
